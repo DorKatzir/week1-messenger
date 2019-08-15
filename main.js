b488e407a6dbd2c212e951c87240e189f77305c5
pagella42 <pagella42@gmail.com>
2019-08-15 10:18:41 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,212 @@
+
+
+
+
+
+const leahcolor = function(){
+    document.getElementById("leah").style.backgroundColor = "#34495e"
+    document.getElementById("leah").style.color = "white"
+    document.getElementById("luis").style.backgroundColor = "#1abc9c"
+    document.getElementById("jona").style.backgroundColor = "#1abc9c"
+    document.getElementById("luis").style.color = "black"
+    document.getElementById("jona").style.color = "black"
+
+    document.getElementById("name").innerHTML = "Leah"
+    document.getElementById("online").innerHTML = "Last online at 14:05"
+    document.getElementById("dot").style.color = "Red"
+    document.getElementById("image").src = "file:///Users/francopagella/Desktop/code/week2/css/leah.jpg"
+                           
+    document.getElementById("txtn1").innerHTML = "Franco are you in the cowork?"
+document.getElementById("txtn2").innerHTML = ""
+document.getElementById("txtn3").innerHTML = ""
+document.getElementById("txtn4").innerHTML = "Need help with some code"
+document.getElementById("txtn5").innerHTML = "could you come"
+document.getElementById("txtn6").innerHTML = ""
+document.getElementById("txtn7").innerHTML = "yeah please"
+document.getElementById("txtn8").innerHTML = ""
+document.getElementById("txtn9").innerHTML = "thank youuu"
+document.getElementById("txtn10").innerHTML = ""
+document.getElementById("txtn11").innerHTML = ""
+
+document.getElementById("txtn12").innerHTML = ""
+document.getElementById("txtn13").innerHTML = "Yeah"
+document.getElementById("txtn14").innerHTML = "why tho?"
+document.getElementById("txtn15").innerHTML = ""
+document.getElementById("txtn16").innerHTML = ""
+document.getElementById("txtn17").innerHTML = "like now?"
+document.getElementById("txtn18").innerHTML = ""
+document.getElementById("txtn19").innerHTML = "ok be there in a minute"
+document.getElementById("txtn20").innerHTML = ""
+document.getElementById("txtn21").innerHTML = "no problem "
+document.getElementById("txtn22").innerHTML = ""
+
+document.getElementById("hr1").innerHTML = "11:55"
+document.getElementById("hr2").innerHTML = ""
+document.getElementById("hr3").innerHTML = ""
+document.getElementById("hr4").innerHTML = "11:59"
+document.getElementById("hr5").innerHTML = "12:00"
+document.getElementById("hr6").innerHTML = ""
+document.getElementById("hr7").innerHTML = "12:01"
+document.getElementById("hr8").innerHTML = ""
+document.getElementById("hr9").innerHTML = "12:06"
+document.getElementById("hr10").innerHTML = ""
+document.getElementById("hr11").innerHTML = ""
+document.getElementById("hr12").innerHTML = ""
+document.getElementById("hr13").innerHTML = "11:56"
+document.getElementById("hr14").innerHTML = "11:56"
+document.getElementById("hr15").innerHTML = ""
+document.getElementById("hr16").innerHTML = ""
+document.getElementById("hr17").innerHTML = "12:01"
+document.getElementById("hr18").innerHTML = ""
+document.getElementById("hr19").innerHTML = "12:05"
+document.getElementById("hr20").innerHTML = ""
+document.getElementById("hr21").innerHTML = "12:10"
+document.getElementById("hr22").innerHTML = ""
+
+
+}
+const jonacolor = function(){
+    document.getElementById("jona").style.backgroundColor = "#34495e"
+    document.getElementById("jona").style.color = "white"
+    document.getElementById("leah").style.backgroundColor = "#1abc9c"
+    document.getElementById("luis").style.backgroundColor = "#1abc9c"
+    document.getElementById("leah").style.color = "black"
+    document.getElementById("luis").style.color = "black"
+
+    document.getElementById("name").innerHTML = "Jona"
+    document.getElementById("online").innerHTML = "Online"
+    document.getElementById("dot").style.color = "Green"
+    document.getElementById("image").src = "./jona.jpg"
+
+    document.getElementById("txtn1").innerHTML = "Guys where are you?"
+document.getElementById("txtn2").innerHTML = ""
+document.getElementById("txtn3").innerHTML = "The lecture has already started"
+document.getElementById("txtn4").innerHTML = "could you come please?"
+document.getElementById("txtn5").innerHTML = ""
+document.getElementById("txtn6").innerHTML = "perfect"
+document.getElementById("txtn7").innerHTML = "we are waiting for you"
+document.getElementById("txtn8").innerHTML = ""
+document.getElementById("txtn9").innerHTML = ""
+document.getElementById("txtn10").innerHTML = ""
+document.getElementById("txtn11").innerHTML = ""
+
+document.getElementById("txtn12").innerHTML = ""
+document.getElementById("txtn13").innerHTML = "why"
+document.getElementById("txtn14").innerHTML = ""
+document.getElementById("txtn15").innerHTML = ""
+document.getElementById("txtn16").innerHTML = "oh sorry, be right there"
+document.getElementById("txtn17").innerHTML = ""
+document.getElementById("txtn18").innerHTML = ""
+document.getElementById("txtn19").innerHTML = ""
+document.getElementById("txtn20").innerHTML = ""
+document.getElementById("txtn21").innerHTML = ""
+document.getElementById("txtn22").innerHTML = ""
+
+document.getElementById("hr1").innerHTML = "14:05"
+document.getElementById("hr2").innerHTML = ""
+document.getElementById("hr3").innerHTML = "14:06"
+document.getElementById("hr4").innerHTML = "14:07"
+document.getElementById("hr5").innerHTML = ""
+document.getElementById("hr6").innerHTML = "14:10"
+document.getElementById("hr7").innerHTML = "14:10"
+document.getElementById("hr8").innerHTML = ""
+document.getElementById("hr9").innerHTML = ""
+document.getElementById("hr10").innerHTML = ""
+document.getElementById("hr11").innerHTML = ""
+
+document.getElementById("hr12").innerHTML = ""
+document.getElementById("hr13").innerHTML = "14:06"
+document.getElementById("hr14").innerHTML = ""
+document.getElementById("hr15").innerHTML = ""
+document.getElementById("hr16").innerHTML = "14:09"
+document.getElementById("hr17").innerHTML = ""
+document.getElementById("hr18").innerHTML = ""
+document.getElementById("hr19").innerHTML = ""
+document.getElementById("hr20").innerHTML = ""
+document.getElementById("hr21").innerHTML = ""
+document.getElementById("hr22").innerHTML = ""
+}
+
+const luiscolor = function(){
+    document.getElementById("luis").style.backgroundColor = "#34495e"
+    document.getElementById("luis").style.color = "white"
+    document.getElementById("jona").style.backgroundColor = "#1abc9c"
+    document.getElementById("leah").style.backgroundColor = "#1abc9c"
+    document.getElementById("leah").style.color = "black"
+    document.getElementById("jona").style.color = "black"
+
+    document.getElementById("name").innerHTML = "Luis"
+    document.getElementById("online").innerHTML = "Online"
+    document.getElementById("dot").style.color = "Green"
+    document.getElementById("image").src = "file:///Users/francopagella/Desktop/code/week2/css/luis.jpeg"
+
+
+    document.getElementById("txtn1").innerHTML = "hey do you wanna eat smt"
+document.getElementById("txtn2").innerHTML = ""
+document.getElementById("txtn3").innerHTML = "omw to the kitchen we got fries"
+document.getElementById("txtn4").innerHTML = ""
+document.getElementById("txtn5").innerHTML = "see ya"
+document.getElementById("txtn6").innerHTML = ""
+document.getElementById("txtn7").innerHTML = ""
+document.getElementById("txtn8").innerHTML = ""
+document.getElementById("txtn9").innerHTML = ""
+document.getElementById("txtn10").innerHTML = ""
+document.getElementById("txtn11").innerHTML = ""
+
+document.getElementById("txtn12").innerHTML = ""
+document.getElementById("txtn13").innerHTML = "yeah whereare you"
+document.getElementById("txtn14").innerHTML = ""
+document.getElementById("txtn15").innerHTML = "nice ill go"
+document.getElementById("txtn16").innerHTML = ""
+document.getElementById("txtn17").innerHTML = ""
+document.getElementById("txtn18").innerHTML = ""
+document.getElementById("txtn19").innerHTML = ""
+document.getElementById("txtn20").innerHTML = ""
+document.getElementById("txtn21").innerHTML = ""
+document.getElementById("txtn22").innerHTML = ""
+
+document.getElementById("hr1").innerHTML = "10:30"
+document.getElementById("hr2").innerHTML = ""
+document.getElementById("hr3").innerHTML = "10:37"
+document.getElementById("hr4").innerHTML = ""
+document.getElementById("hr5").innerHTML = "10:40"
+document.getElementById("hr6").innerHTML = ""
+document.getElementById("hr7").innerHTML = ""
+document.getElementById("hr8").innerHTML = ""
+document.getElementById("hr9").innerHTML = ""
+document.getElementById("hr10").innerHTML = ""
+document.getElementById("hr11").innerHTML = ""
+
+document.getElementById("hr12").innerHTML = ""
+document.getElementById("hr13").innerHTML = "10:35"
+document.getElementById("hr14").innerHTML = ""
+document.getElementById("hr15").innerHTML = "10:40"
+document.getElementById("hr16").innerHTML = ""
+document.getElementById("hr17").innerHTML = ""
+document.getElementById("hr18").innerHTML = ""
+document.getElementById("hr19").innerHTML = ""
+document.getElementById("hr20").innerHTML = ""
+document.getElementById("hr21").innerHTML = ""
+document.getElementById("hr22").innerHTML = ""
+
+}
+
+
+const erase = function(){
+    document.getElementById("bar").value = ""
+}
+
+const lookup = function(){
+    
+
+    if(document.getElementById("search").value == "luis"){
+        luiscolor()
+    }
+    if(document.getElementById("search").value == "leah"){
+        leahcolor()
+    }
+    if(document.getElementById("search").value == "jona"){
+        jonacolor()
+    }
+    document.getElementById("search").value = ""
+}
